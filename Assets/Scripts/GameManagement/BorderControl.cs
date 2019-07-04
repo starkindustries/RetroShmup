7e86578cb82cda9932a3362feeb45ec1f2e6bee1
Zion Perez <zionperez@gmail.com>
2019-07-04 21:00:44 +0900

    Fix gameOver screen bug; add a check in border control to ignore the player tag.

--- Assets/Scripts/GameManagement/BorderControl.cs
+++ Assets/Scripts/GameManagement/BorderControl.cs
@@ -14,6 +14,13 @@ public class BorderControl : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("BorderControl detected: " + collision.name);
+        // Ignore trigger if it was the player
+        // This is necessary because currently the player falls downwards and hits the boundary on death
+        // The boundary destroys the player object before it can call GameOver()
+        if (collision.tag == "Player")
+        {
+            return;
+        }
         Destroy(collision.gameObject);
     }    
 }
